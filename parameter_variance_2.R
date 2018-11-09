78372b3892e179734bd0b55532d594ea880806c8
Maximilian Maehr <iza25929@ad.iza.org>
2018-11-09 17:45:52 +0100

    Added another, faster paramter variance estimator.

---
+++ parameter_variance_2.R
@@ -0,0 +1,23 @@
+parameter_variance <- function(X, fac, load) {
+  c(t, i, p) %<-% dim(X)
+  
+  #compute projection matrix
+  
+  M_F <- diag(t) - fac %*% solve(t(fac) %*% fac) %*% t(fac)
+  M_L <- diag(i) - load %*% solve(t(load) %*% load) %*% t(load)
+  
+  #compute variance matrix
+  
+  D_F <- matrix(0, nrow = p, ncol = p)
+  
+  for (j in 1:p) {
+    for (k in 1:p) {
+      D_F[j, k] <- tr(M_L %*% t(X[,,j]) %*% M_F %*% X[,,k])
+    }
+  }
+  
+  D_F <- 1 / (t * i) * solve(1 / (t * i) * D_F) #first term cause of root TN consistency, p. 1241
+  
+  return(D_F)
+  
+}
