8c706bac6b65f248b02a51b45fe54de0ae007061
Maximilian Maehr <iza25929@ad.iza.org>
2018-11-09 15:25:57 +0100

    Function to compute error terms, either serial- or cross-correlation or both.

---
+++ error_function.R
@@ -0,0 +1,49 @@
+error_function <- function(t, i, mean = 0, sd = 1, cross_corr = FALSE, serial_corr = FALSE, rho = NA) {
+  
+  epsilon <- matrix(0, nrow = t, ncol = i)
+  
+  if(((cross_corr | serial_corr) & is.na(rho)) == TRUE) {
+    stop('You need to specify rho.')
+  }
+  
+  if(cross_corr == TRUE) {
+    for (k in 1:i) {
+      if (k == 1) {
+        epsilon[, k] <- rnorm(t, mean = mean, sd = sd)
+      }
+      else {
+        epsilon[, k] <-
+          rho * epsilon[, k - 1] + rnorm(t, mean = mean, sd = sd)
+      }
+    } 
+    } else if (serial_corr == TRUE) {
+    for (k in 1:t) {
+      if (k == 1) {
+        epsilon[k,] <- rnorm(i, mean = mean, sd = sd)
+      }
+      else {
+        epsilon[k,] <-
+          rho * epsilon[k - 1,] + rnorm(i, mean = mean, sd = sd)
+      }
+    }
+    } else if (cross_corr == TRUE & serial_corr == TRUE) {
+    epsilon[1,] <- rnorm(i, mean = mean, sd = sd)
+    epsilon[,1] <- rnorm(t, mean = mean, sd = sd)
+    for (k in 2:t) {
+      for (j in 2:i) {
+        epsilon[k, j] <-
+          rho * epsilon[k - 1, j - 1] + rnorm(mean = mean, sd = sd)
+      }
+    }
+    } else {
+    epsilon <- rnorm(t * i, mean = mean, sd = sd)
+  }
+
+  return(epsilon)
+  
+}
+
+
+a <- error_function(10, 100, mean = 1, sd = 2, serial_corr = TRUE)
+sd(a)
+
