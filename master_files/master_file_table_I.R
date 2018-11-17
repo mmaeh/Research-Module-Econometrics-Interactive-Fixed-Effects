0032fd7df183b6216ee275dec8848bb492ac135f
Maximilian Mähr <maximilian.maehr@gmail.com>
2018-11-17 19:40:53 +0100

    Automatically loads all functions in /functions directory.

---
+++ master_files/master_file_table_I.R
@@ -0,0 +1,79 @@
+#load packages
+library('phtt')
+library('psych')
+library('zeallot')
+
+#functions used in the following
+function.sources <- paste('./functions/', list.files('./functions/', pattern="*.R"), sep = '')
+sapply(function.sources, source, .GlobalEnv)
+
+#combinations of i and t to use in simulation
+combinations <- list(
+  c(100, 10),
+  c(100, 20),
+  c(100, 50),
+  c(100, 100),
+  c(10, 100),
+  c(20, 100),
+  c(50, 100)
+)
+
+#parameters and constants used
+param <- c(1, 3, 5, 2, 4)
+r <- 2
+m <- 1000
+
+#container to store results
+temp_results <- data.frame(matrix(nrow = m, ncol = 22))
+results <- data.frame(matrix(nrow = length(combinations), ncol = 22))
+colnames(results) <- c('i', 't', 'coef1_ols', 'coef2_ols', 'sd1_ols', 'sd2_ols', 
+                       'coef1_within', 'coef2_within', 'sd1_within', 'sd2_within',
+                       'coef1_infeasible', 'coef2_infeasible', 'sd1_infeasible', 'sd2_infeasible', 'sigma_infeasible', 'df1_inf', 'df2_inf',
+                       'coef1_interactive', 'coef2_interactive', 'sd1_interactive', 'sd2_interactive', 'sigma_interactive', 'df1_int', 'df2_int')
+
+
+#--------------------------------------------
+#start simulation process--------------------
+#--------------------------------------------
+
+
+progress <- txtProgressBar(min = 0, max = length(combinations), style = 3)
+iter <- 0
+
+for (c in combinations) {
+  
+  c(i, t) %<-% combinations[[iter + 1]]
+  results[iter + 1, 1:2] %<-% c(i, t)
+  
+  for (j in 1:m) {
+    
+    #create simulated data
+    
+    epsilon <- error_function(t, i, mean = 0, sd = 2)
+    source('data_generating2.R')
+    
+    #estimate naive-estimator
+    
+    #temp_results[j, 1:4] <- within_est(X, Y, individual = FALSE, time = FALSE)
+    
+    #estimate within-estimator
+    
+    #temp_results[j, 1:4] <- within_est_2(X, Y, individual = TRUE, time = TRUE) #coeff are right!
+    
+    #estimate infeasible-estimator
+    
+    #temp_results[j, 9:12] <- infeasible_est(X, Y, given = "factors") #coeff seem right as well
+    
+    #estimate interactive-estimator
+    
+    #temp_results[j, 3:7] <- interactive_est(X, Y, r, 0.0001)
+    temp_results[j, 8:11] <- Eup(Y ~ X[,,1] + X[,,2] + X[,,4] + X[,,5], factor.dim = 2)$slope.para
+    
+  }
+  
+  results[iter + 1, 3:22] <- colMeans(temp_results, na.rm = TRUE)  
+  
+  iter <- iter + 1 
+  setTxtProgressBar(progress, iter)
+  
+}
