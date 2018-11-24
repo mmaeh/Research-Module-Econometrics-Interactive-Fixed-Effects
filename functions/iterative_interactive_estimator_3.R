b3a47826145ab1a2f5c67245db5ebd9f9ea7e4dd
Maximilian Mähr <maximilian.maehr@gmail.com>
2018-11-24 13:50:08 +0100

    Interactive estimator returning lowest SSR form two starting values (OLS and PCA).

---
+++ functions/iterative_interactive_estimator_3.R
@@ -0,0 +1,56 @@
+interactive_est_3 <-
+  function(X, Y, r, tolerate) {
+    # dimensions of X
+    c(t, i, p) %<-% dim(X)
+    
+    # optimize containers
+    beta_start <- matrix(0, nrow = 2, ncol = p)
+    beta_start[2, ] <- within_est(X, Y, individual = FALSE, time = FALSE)
+    
+    # things to intialize before while loop
+    xxinv <- xx_inv(X)
+    beta_opt <- matrix(NA, nrow = 2, ncol = p)
+    sigma_opt <- matrix(NA, nrow = 2, ncol = 1)
+
+    for (s in 1:2) {
+
+      beta <- beta_start[s,]
+      betaold <- beta
+      betanorm <- 1
+      n <- 0
+  
+      
+      # while loop to get interactive estimator 
+      while (n < 500 && betanorm > tolerate) {
+        
+        # get factor and loading estimates
+        W <- Y
+        for (h in 1:p) {
+          W <- W - X[, , h] * beta[h]
+        }
+        c(fac_hat, load_hat, VNT) %<-% factor_est(W, r)
+        
+        # update beta and compare to previous estimate
+        beta <- beta_update(X, xxinv, Y, fac_hat, load_hat)
+        betanorm <- norm(beta - betaold)
+        betaold <- beta
+        
+        # set counter + 1
+        n <- n + 1
+        
+      }
+      
+      err <- Y - fac_hat %*% t(load_hat)
+      for (n in 1:length(param)) {
+        err <- err - X[,,n] * beta[n]
+      }
+      
+      sigma_opt[s] <- tr(err %*% t(err)) 
+      
+      beta_opt[s,] <- beta
+      
+    }
+      
+    return(beta_opt[which.min(sigma_opt),])
+    
+  }
