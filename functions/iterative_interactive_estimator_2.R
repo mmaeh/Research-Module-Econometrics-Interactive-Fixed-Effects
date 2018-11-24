fb495bd50b967cf5399f00be3679c3962547dd15
Maximilian Mähr <maximilian.maehr@gmail.com>
2018-11-24 13:53:08 +0100

    Optimized interactive estimator.

--- functions/iterative_interactive_estimator_2.R
+++ functions/iterative_interactive_estimator_2.R
@@ -1,35 +1,50 @@
 interactive_est_2 <-
-  function(X, Y, r, tolerate) {
+  function(X, Y, r, beta_start = "factor", tolerate) {
+    # dimensions of X
     c(t, i, p) %<-% dim(X)
     
+    # initialize beta values
+    if (beta_start == "OLS") {
+      beta <- within_est(X, Y, individual = FALSE, time = FALSE)
+    } else if (beta_start == "within") {
+      beta <- within_est(X, Y, individual = TRUE, time = TRUE)
+    } else {
+      beta <- matrix(0, nrow = p)
+    }
+    
+    betaold <- beta
+
+    # things to intialize before while loop
+    xxinv <- xx_inv(X)        
     betanorm <- 1
-    betaold <- matrix(0, nrow = p, ncol = 1)
-    
-    xxinv <- xx_inv(X)
-    
-    W <- Y
-    c(fac_hat, load_hat, VNT) %<-% factor_est(W, r)
-    
     n <- 0
+    
+    # while loop to get interactive estimator 
     while (n < 500 && betanorm > tolerate) {
-      n <- n + 1
-      beta <- beta_update(X, xxinv, Y, fac_hat, load_hat)
-      betanorm <- norm(beta - betaold)
-      betaold <- beta
-      W <- Y
       
+      # get factor and loading estimates
+      W <- Y
       for (h in 1:p) {
         W <- W - X[, , h] * beta[h]
       }
-      
       c(fac_hat, load_hat, VNT) %<-% factor_est(W, r)
+      
+      # update beta and compare to previous estimate
+      beta <- beta_update(X, xxinv, Y, fac_hat, load_hat)
+      betanorm <- norm(beta - betaold)
+      betaold <- beta
+      
+      # set counter + 1
+      n <- n + 1
     
     }
       
-    err <- Y - fac_hat %*% t(load_hat)
+    "err <- Y - fac_hat %*% t(load_hat)
     for (n in 1:length(param)) {
       err <- err - X[,,n] * beta[n]
     }
+    
+    sigma <- tr(err %*% t(err))
     
     sigma <- tr(err %*% t(err)) / ((i - r) * (t - r) - p)
     
@@ -37,8 +52,8 @@ interactive_est_2 <-
     D_F_diag <- diag(D_F)
     
     variance <- sigma * D_F
-    sd <- sqrt(diag(variance))
+    sd <- sqrt(diag(variance))"
     
-    return(c(beta, sd))
+    return(beta)
     
   }
