1187039fdae06eaa08938f0594652bba531499fc
Maximilian Mähr <maximilian.maehr@gmail.com>
2019-01-22 10:24:59 +0100

    Newest version.

---
+++ paper_tables/6_unit_root.R
@@ -0,0 +1,111 @@
+# Load necessary packages.
+library(phtt)
+library(psych)
+library(xlsx)
+library(xtable)
+library(zeallot)
+
+# Load necessary functions used in the simulation.
+function.sources <- paste('./functions/', list.files('./functions/', pattern = "*.R"), sep = '')
+sapply(function.sources, source, .GlobalEnv)
+
+# Combinations of N and T used in the simulation.
+combinations <- list(
+  c(100, 3),
+  c(100, 5),
+  c(100, 10),
+  c(100, 20),
+  c(100, 50),
+  c(100, 100),
+  c(3, 100),
+  c(5, 100),
+  c(10, 100),
+  c(20, 100),
+  c(50, 100)
+)
+
+# Used constants.
+param <- c(1, 3)
+R <- 2
+n_rep <- 1000
+
+# Container to store results.
+temp_results <- data.frame(matrix(nrow = n_rep, ncol = 12))
+results <-
+  data.frame(matrix(nrow = length(combinations), ncol = 14))
+colnames(results) <-
+  c(
+    'N',
+    'T',
+    'coef1_ols',
+    'sd1_ols',
+    'coef2_ols',
+    'sd2_ols',
+    'coef1_within',
+    'sd1_within',
+    'coef2_within',
+    'sd2_within',
+    'coef1_IEF',
+    'sd1_IEF',
+    'coef2_IEF',
+    'sd2_IEF'
+  )
+
+
+#--------------------------------------------
+# Start simulation process-------------------
+#--------------------------------------------
+
+# Set up progress bar.
+progress <-
+  txtProgressBar(min = 0,
+                 max = length(combinations) * n_rep,
+                 style = 3)
+iter <- 0
+
+for (c in combinations) {
+  c(N, T) %<-% combinations[[iter + 1]]
+  results[iter + 1, 1:2] %<-% c(N, T)
+  
+  for (rep in 1:n_rep) {
+    #create simulated data
+    
+    epsilon <- error_function(T, N, mean = 0, sd = 2)
+    source('./data_generating/data_generating_unit_root.R')
+    
+    #estimate naive-estimator
+    
+    temp_results[rep, c(1, 3)] <-
+      within_est(X, Y, individual = FALSE, time = FALSE)
+    
+    #estimate within-estimator
+    
+    temp_results[rep, c(5, 7)] <-
+      within_est(X, Y, individual = TRUE, time = TRUE)
+    
+    #estimate interactive-estimator
+    
+    temp_results[rep, c(9, 11)] <- 
+      interactive_est_3(X, Y, R, 0.0001)
+    
+    
+    setTxtProgressBar(progress, iter * n_rep + rep)
+    
+  }
+  
+  for (k in seq(2, 12, 2)) {
+    temp_results[, k] <-
+      sqrt(mean((temp_results[, k - 1] - mean(temp_results[, k - 1])) ^ 2))
+  }
+  
+  results[iter + 1, 3:14] <- colMeans(temp_results, na.rm = TRUE)
+  
+  iter <- iter + 1
+  
+}
+
+saveRDS(results, './output/table_unit_root.rds')
+write.csv(results, './output/table_unit_root.csv')
+
+table <- xtable(results, digits = 3, auto = TRUE)
+print.xtable(table, include.rownames = FALSE)
